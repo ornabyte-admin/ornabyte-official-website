775cb9706fc3cb549d19581e4b3e2862e1120a04
Arjun Saji <ajusaji11@gmail.com>
2025-07-20 02:25:49 +0530

    💡 Fixed portfolio image issue on mobile view,basic corrections of the page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,87 +1,126 @@
-
 $(function () {
+  "use strict";
 
-    "use strict";
+  $(function () {
+    var equalWidth = $(".clients-sa .item").outerWidth();
 
-    
-    $(function () {
-
-        var equalWidth = $(".clients-sa .item").outerWidth();
-
-        $(".clients-sa .item").css({
-            "height": equalWidth
-        });
-
-    }); 
-
-    var testim = new Swiper(".testimonials-sa .testim-swiper", {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        speed: 1500,
-        autoplay: {
-            delay: 5000,
-        },
-        loop: true,
-        pagination: {
-            el: ".testimonials-sa .swiper-pagination",
-            clickable: true,
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 1,
-            },
-            640: {
-                slidesPerView: 1,
-            },
-            768: {
-                slidesPerView: 2,
-            },
-            1024: {
-                slidesPerView: 3,
-            },
-        }
+    $(".clients-sa .item").css({
+      height: equalWidth,
     });
+  });
 
+  var testim = new Swiper(".testimonials-sa .testim-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    speed: 1500,
+    autoplay: {
+      delay: 5000,
+    },
+    loop: true,
+    pagination: {
+      el: ".testimonials-sa .swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
 });
 
+// code added by arjun for the animation issue in the homepage -arjun 20/07/2025
 
 $(function () {
-    var width = $(window).width();
-    if (width > 991) {
+  "use strict";
 
-        "use strict";
+  ScrollTrigger.matchMedia({
+    "(min-width: 450px)": function () {
+      let cards = gsap.utils.toArray(".cards .card-item");
 
-        $(function () {
-            let cards = gsap.utils.toArray(".cards .card-item");
-    
-            let stickDistance = 0;
-    
-            let firstCardST = ScrollTrigger.create({
-                trigger: cards[0],
-                start: "center center"
-            });
-    
-            let lastCardST = ScrollTrigger.create({
-                trigger: cards[cards.length - 1],
-                start: "bottom bottom"
-            });
-    
-            cards.forEach((card, index) => {
-                var scale = 1 - (cards.length - index) * 0.025;
-                let scaleDown = gsap.to(card, { scale: scale, 'transform-origin': '"50% ' + (lastCardST.start + stickDistance) + '"' });
-    
-                ScrollTrigger.create({
-                    trigger: card,
-                    start: "center center",
-                    end: () => lastCardST.start + stickDistance,
-                    pin: true,
-                    pinSpacing: false,
-                    ease: "none",
-                    animation: scaleDown,
-                    toggleActions: "restart none none reverse"
-                });
-            });
+      let stickDistance = 0;
+
+      let firstCardST = ScrollTrigger.create({
+        trigger: cards[0],
+        start: "center center",
+      });
+
+      let lastCardST = ScrollTrigger.create({
+        trigger: cards[cards.length - 1],
+        start: "bottom bottom",
+      });
+
+      cards.forEach((card, index) => {
+        var scale = 1 - (cards.length - index) * 0.025;
+
+        let scaleDown = gsap.to(card, {
+          scale: scale,
+          transformOrigin: "50% " + (lastCardST.start + stickDistance),
         });
 
-    }
+        ScrollTrigger.create({
+          trigger: card,
+          start: "center center",
+          end: () => lastCardST.start + stickDistance,
+          pin: true,
+          pinSpacing: false,
+          ease: "none",
+          animation: scaleDown,
+          toggleActions: "restart none none reverse",
+        });
+      });
+    },
+  });
 });
+
+// the code is commented because it will not work in the mobile screens thats why
+
+// $(function () {
+//     var width = $(window).width();
+//     if (width > 991) {
+
+//         "use strict";
+
+//         $(function () {
+//             let cards = gsap.utils.toArray(".cards .card-item");
+
+//             let stickDistance = 0;
+
+//             let firstCardST = ScrollTrigger.create({
+//                 trigger: cards[0],
+//                 start: "center center"
+//             });
+
+//             let lastCardST = ScrollTrigger.create({
+//                 trigger: cards[cards.length - 1],
+//                 start: "bottom bottom"
+//             });
+
+//             cards.forEach((card, index) => {
+//                 var scale = 1 - (cards.length - index) * 0.025;
+//                 let scaleDown = gsap.to(card, { scale: scale, 'transform-origin': '"50% ' + (lastCardST.start + stickDistance) + '"' });
+
+//                 ScrollTrigger.create({
+//                     trigger: card,
+//                     start: "center center",
+//                     end: () => lastCardST.start + stickDistance,
+//                     pin: true,
+//                     pinSpacing: false,
+//                     ease: "none",
+//                     animation: scaleDown,
+//                     toggleActions: "restart none none reverse"
+//                 });
+//             });
+//         });
+
+//     }
+// });
